0fda8aa8417e2c077a2034cd01c6cdcc4ac75728
Bartłomiej <bartlomiejgluch@gmail.com>
2018-09-26 15:01:56 +0200

    Init stack

--- src/main/java/pl/projects/bartek/mystack/MyStack.java
+++ src/main/java/pl/projects/bartek/mystack/MyStack.java
@@ -2,21 +2,35 @@ package pl.projects.bartek.mystack;
 
 import java.util.function.Function;
 
-public class MyStack<Type> implements Stack {
+public class MyStack<Type> implements Stack<Type> {
+
+    private Function<Integer, Type[]> provider;
+    private Type[] array;
+    private int lenght;
+    private int sizeIncrase;
+
+    public MyStack(int size, int sizeIncrase, Function<Integer, Type[]> provider) {
+
+        this.provider = provider;
+        array = provider.apply(size);
+        this.sizeIncrase = sizeIncrase;
+        lenght = 0;
+
+    }
 
 
-     @Override
-     public void push(Object a) {
+    @Override
+    public void push(Type a) {
 
-     }
+    }
 
-     @Override
-     public Object pop() {
-          return null;
-     }
+    @Override
+    public Type pop() {
+        return null;
+    }
 
-     @Override
-     public boolean isEmpty() {
-          return false;
-     }
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
