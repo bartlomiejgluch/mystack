76f5dfbb6d333ae83192b31daa2c391ea1405884
Bartłomiej <bartlomiejgluch@gmail.com>
2018-09-26 16:09:12 +0200

    Add tests

--- src/test/java/pl/projects/bartek/mystack/MyStackTest.java
+++ src/test/java/pl/projects/bartek/mystack/MyStackTest.java
@@ -2,14 +2,42 @@ package pl.projects.bartek.mystack;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
 public class MyStackTest {
 
 
     @Test
     public void testPush() {
 
+        Stack<Integer> myStack = new MyStack<>(10, 5, Integer[]::new);
 
+        myStack.push(4);
+
+        assertEquals("[4]", String.valueOf(myStack));
     }
 
+    @Test
+    public void testPop() {
+
+        Stack<Integer> myStack = new MyStack<>(10, 5, Integer[]::new);
+
+        myStack.push(4);
+        myStack.pop();
+
+        assertEquals("[]", String.valueOf(myStack));
+    }
+
+    @Test
+    public void testisEmpty() {
+
+        Stack<Integer> myStack = new MyStack<>(10, 5, Integer[]::new);
+
+        myStack.push(4);
+        myStack.pop();
+
+        assertTrue(myStack.isEmpty());
+    }
 
 }
