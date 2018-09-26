8688ca84a84a35dac9b742e7383a594b04427c7a
Bartłomiej <bartlomiejgluch@gmail.com>
2018-09-26 16:20:36 +0200

    Add tests peek

--- src/test/java/pl/projects/bartek/mystack/MyStackTest.java
+++ src/test/java/pl/projects/bartek/mystack/MyStackTest.java
@@ -40,4 +40,30 @@ public class MyStackTest {
         assertTrue(myStack.isEmpty());
     }
 
+    @Test
+    public void testPeek1() {
+
+        Stack<Integer> myStack = new MyStack<>(10, 5, Integer[]::new);
+
+        myStack.push(4);
+        myStack.push(56);
+        myStack.push(45);
+
+
+        assertEquals("45", String.valueOf(myStack.pop()));
+    }
+
+    @Test
+    public void testPeek2() {
+
+        Stack<Integer> myStack = new MyStack<>(10, 5, Integer[]::new);
+
+        myStack.push(4);
+        myStack.push(56);
+        myStack.push(45);
+        myStack.pop();
+
+        assertEquals("56", String.valueOf(myStack.pop()));
+    }
+
 }
